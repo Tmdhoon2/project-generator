1661559c4959d8efaed650415c359a17fd7e2a81
Tmdhoon2 <hks026naver@gmail.com>
2024-04-19 17:28:30 +0900

    chore :: ktor

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -5,10 +7,39 @@ plugins {
 
 apply<CommonGradlePlugin>()
 
+val properties = Properties()
+properties.load(rootProject.file("./local.properties").inputStream())
+
 android {
     namespace = "com.seunghoon.core.network"
+
+    buildTypes {
+        release {
+            buildConfigField(
+                type = "String",
+                name = "BASE_URL",
+                value = properties.getProperty("BASE_URL_PROD", "\"\""),
+            )
+        }
+        debug {
+            buildConfigField(
+                type = "String",
+                name = "BASE_URL",
+                value = properties.getProperty("BASE_URL_DEV", "\"\""),
+            )
+        }
+    }
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
     api(project(":core:common"))
+
+    api(libs.io.ktor.android)
+    api(libs.io.ktor.serialization)
+    api(libs.io.ktor.serialization.json)
+    api(libs.io.ktor.logging)
+    api(libs.io.ktor.content.negotiation)
 }
